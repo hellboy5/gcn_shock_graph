e5cc8d96eefc5f45825683dff00f762f1a3d6cb9
Narayanan, Maruthi <maruthi.narayanan@jhuapl.edu>
2019-02-04 15:57:20 -0500

    :tada: new unit tests

---
+++ tests/unit_test.py
@@ -0,0 +1,35 @@
+from keras.models import Input, Model, Sequential
+from keras.layers import Dense, Activation, Dropout, Lambda
+from keras.utils import plot_model
+import keras.backend as K
+import numpy as np
+import sys
+import h5py
+
+sys.path.append('/Users/naraym1/work/gcn_shock_graph')
+
+from datagen.sg_graph_datagen import ShockGraphDataGenerator
+
+train_dir='/Users/naraym1/work/cifar_100/unit_train_dir'
+label_file='/Users/naraym1/work/cifar_100/scripts/labels.txt'
+
+# Parameters
+params = {'numb_nodes': 294,
+          'numb_attrs': 19,
+          'batch_size': 100,
+          'n_classes': 100,
+          'shuffle': False}
+
+#Generators
+training_generator=ShockGraphDataGenerator(train_dir,label_file,**params)
+
+
+batch,labels=training_generator[0]
+
+hf = h5py.File('test.h5','w')
+
+
+hf.create_dataset('batch',data=batch[1])
+hf.create_dataset('labels',data=labels)
+
+hf.close()
---
+++ tests/unit_test_matlab.m
@@ -0,0 +1,35 @@
+clear all
+close all
+
+unit_test_dir='/Users/naraym1/work/cifar_100/unit_train_dir';
+
+files=dir([unit_test_dir '/*.h5']);
+
+nodes=294;
+gt_batch=zeros(nodes,nodes,100);
+
+for f=1:length(files)
+    fname=[files(f).folder '/' files(f).name];
+    F = h5read(fname,'/feature').';
+    A = h5read(fname,'/adj_matrix').';
+    diff=nodes-size(A,1);
+    pad_A=padarray(A,[diff diff],0,'post');
+    pad_A=pad_A+eye(nodes);
+    D=diag(sum(pad_A,2).^(-1/2));
+    norm_adj=D*pad_A*D;
+    gt_batch(:,:,f)=norm_adj;
+end
+
+
+test_batch=h5read('test.h5','/batch');
+test_labels=h5read('test.h5','/labels');
+
+max_diff=zeros(size(gt_batch,3),1);
+for z=1:size(gt_batch,3)
+   max_diff(z)=max(max(abs(gt_batch(:,:,z)-test_batch(:,:,z))));
+   
+
+    
+end
+
+isequal(test_labels,eye(length(files)))
