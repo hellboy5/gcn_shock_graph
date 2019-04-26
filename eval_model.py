6ebea11f779a30da94d1d80e0a5f77bc4b100c9a
Maruthi Narayanan <maruthi.narayanan@gmail.com>
2019-04-26 14:10:30 -0400

    :zap: updated classes to deal with new sg model class

--- eval_model.py
+++ eval_model.py
@@ -8,13 +8,32 @@ from models.gcn_sg_model import Classifier
 from torch.utils.data import DataLoader
 from functools import partial
 
-def eval(model,test_dir,dataset,cache_io,symm_io,shuffle_io):
+def eval(config_file,state_path,device):
+
+    
+    test_dir=config_file['test_dir']
+    dataset=config_file['dataset']
+    cache_io=config_file['cache_io']
+    symm_io=config_file['symm_io']
+    shuffle_io=config_file['shuffle_io']
+    num_classes=config_file['num_classes']
+    input_dim=config_file['features_dim']
+    hidden_dim=config_file['hidden_dim']
+    hidden_layers=config_file['hidden_layers']
+    aggregate=config_file['aggregate']
+    combine=config_file['combine']
+    batch_io=config_file['batch']
+
+    model = Classifier(input_dim, hidden_dim, num_classes,hidden_layers,aggregate,combine,device)
+    model.load_state_dict(torch.load(state_path)['model_state_dict'])
+    model.to(device)
+    model.eval()
 
     testset=ShockGraphDataset(test_dir,dataset,cache=cache_io,symmetric=symm_io,data_augment=False)
 
     # Use PyTorch's DataLoader and the collate function
     # defined before.
-    data_loader = DataLoader(testset, batch_size=32, shuffle=shuffle_io,
+    data_loader = DataLoader(testset, batch_size=batch_io, shuffle=shuffle_io,
                              collate_fn=partial(collate,device_name=device))
 
     predicted=torch.LongTensor()
@@ -42,22 +61,11 @@ if __name__ == "__main__":
     
     cfg=sys.argv[1]
     config_file=json.load(open(cfg))
-    test_dir=config_file['test_dir']
-    dataset=config_file['dataset']
-    cache_io=config_file['cache_io']
-    symm_io=config_file['symm_io']
-    shuffle_io=config_file['shuffle_io']
-    input_dim=config_file['features_dim']
-    num_classes=config_file['num_classes']
-    
+
     device_id=sys.argv[2]
     device="cuda:"+str(device_id)
     
     state_path=sys.argv[3]
-    model = Classifier(input_dim, 512, num_classes,device)
-    model.load_state_dict(torch.load(state_path)['model_state_dict'])
-    model.to(device)
-    model.eval()
 
-    eval(model,test_dir,dataset,cache_io,symm_io,shuffle_io)
+    eval(config_file,state_path,device)
     
--- train_model.py
+++ train_model.py
@@ -11,25 +11,46 @@ from torch.utils.data import DataLoader
 from functools import partial
 
 
-def train(train_dir,dataset,cache_io,symm_io,shuffle_io,num_classes,input_dim,device):
-    epochs=400
-    hidden_dim=512
-    batch_io=64
-    
-    trainset=ShockGraphDataset(train_dir,dataset,cache=cache_io,symmetric=symm_io,data_augment=True)
+def train(config_file,device):
 
+    train_dir=config_file['train_dir']
+    dataset=config_file['dataset']
+    cache_io=config_file['cache_io']
+    symm_io=config_file['symm_io']
+    shuffle_io=config_file['shuffle_io']
+    num_classes=config_file['num_classes']
+    input_dim=config_file['features_dim']
+    hidden_dim=config_file['hidden_dim']
+    hidden_layers=config_file['hidden_layers']
+    aggregate=config_file['aggregate']
+    combine=config_file['combine']
+    epochs=config_file['epochs']
+    batch_io=config_file['batch']
+    apply_da=config_file['data_augment']
+
+    prefix='gcn_sg_model_'+dataset+'_'+aggregate+'_'+combine+'_'+str(hidden_dim)+'_'+str(hidden_layers)
+    
+    print("Training with batch size of: ",batch_io," over ",epochs," Epochs with da: ",apply_da)
+    print("Writing out to : ",prefix)
+    
+    trainset=ShockGraphDataset(train_dir,dataset,cache=cache_io,symmetric=symm_io,data_augment=apply_da)
+
+
+    
     # Use PyTorch's DataLoader and the collate function
     # defined before.
     data_loader = DataLoader(trainset, batch_size=batch_io, shuffle=shuffle_io,
                              collate_fn=partial(collate,device_name=device))
     
     # Create model
-    model = Classifier(input_dim, hidden_dim, num_classes,device)
+    model = Classifier(input_dim, hidden_dim, num_classes,hidden_layers,aggregate,combine,device)
     loss_func = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=0.001)#,weight_decay=0.00005/2.0)
     model.to(device)
     model.train()
-
+    
+    print(model)
+    
     epoch_losses = []
     for epoch in tqdm(range(epochs)):
         epoch_loss = 0
@@ -45,7 +66,7 @@ def train(train_dir,dataset,cache_io,symm_io,shuffle_io,num_classes,input_dim,de
         print('Epoch {}, loss {:.6f}'.format(epoch, epoch_loss))
 
         if (epoch+1) % 50 == 0:
-            path='gcn_sg_model_'+dataset+'_epoch_'+str(epoch+1).zfill(3)+'.pth'
+            path=prefix+'_epoch_'+str(epoch+1).zfill(3)+'.pth'
             torch.save({
                 'epoch': epoch,
                 'model_state_dict': model.state_dict(),
@@ -64,15 +85,8 @@ if __name__ == "__main__":
     
     cfg=sys.argv[1]
     config_file=json.load(open(cfg))
-    train_dir=config_file['train_dir']
-    dataset=config_file['dataset']
-    cache_io=config_file['cache_io']
-    symm_io=config_file['symm_io']
-    shuffle_io=config_file['shuffle_io']
-    num_classes=config_file['num_classes']
-    input_dim=config_file['features_dim']
 
     device_id=sys.argv[2]
     device="cuda:"+str(device_id)
-    train(train_dir,dataset,cache_io,symm_io,shuffle_io,num_classes,input_dim,device)
+    train(config_file,device)
     
