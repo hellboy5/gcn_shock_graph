ec9ad50a277c77e97c81bad7eb40db366e764273
Narayanan, Maruthi <maruthi.narayanan@jhuapl.edu>
2019-10-08 23:53:21 -0400

    :sparkles: added new edge shape features

--- utils/shock_graph_convert.py
+++ utils/shock_graph_convert.py
@@ -4,6 +4,7 @@ import os
 import scipy.misc
 import numpy as np
 import h5py
+import math
 
 from collections import defaultdict
 from collections import namedtuple
@@ -16,12 +17,192 @@ from scipy.spatial.distance import cdist
 
 node_order=dict()
 node_mapping=dict()
+edge_samples=dict()
+edge_to_samples=dict()
+curve_stats=dict()
 samp_to_node_mapping=defaultdict(list)
 edge_mapping=defaultdict(list)
 adj_nodes_mapping=defaultdict(list)
 Sample=namedtuple('Sample','pt type radius theta phi plus_pt \
 minus_pt plus_theta minus_theta')
+CurveProps=namedtuple('CurveProps','SCurve SLength SAngle PCurve PLength PAngle MCurve MLength MAngle PolyArea')
 highest_degree=0
+ZERO_TOLERANCE=1E-1
+
+def computeDerivatives(curve):
+
+     #Compute derivatives
+     dx=[]
+     dx.append(0.0)
+     dy=[]
+     dy.append(0.0)
+
+     px=curve[0][0]
+     py=curve[0][1]
+
+     for i in range(1,len(curve)):
+          cx=curve[i][0]
+          cy=curve[i][1]
+          dL=np.hypot(cx-px,cy-py)
+
+          if dL > ZERO_TOLERANCE: 
+               dx.append((cx-px)/dL)
+               dy.append((cy-py)/dL)
+          else:
+               dx.append(0.0)
+               dy.append(0.0)
+    
+          px=cx
+          py=cy
+
+     return dx,dy
+
+def computeArcLength(curve):
+
+     #Compute arc length and normalized arc length
+     arcLength=[]
+     s=[]
+     length=0
+     arcLength.append(0.0)
+     s.append(0.0)
+
+     px=curve[0][0]
+     py=curve[0][1]
+
+     for i in range(1,len(curve)):          
+          cx=curve[i][0]
+          cy=curve[i][1]
+          dL = np.hypot(cx-px,cy-py)
+          length += dL
+          arcLength.append(length)
+          s.append(dL)
+          px=cx
+          py=cy
+
+     return arcLength,length
+
+  
+
+
+def computeCurvatures(dx,dy,arcLength,curveLength):
+     
+     #Compute curvature
+     curvature=[]
+     curvature.append(0.0)
+     totalCurvature=0.0
+
+     for i in range(1,curveLength):
+          pdx=dx[i-1]
+          pdy=dy[i-1]
+          cdx=dx[i]
+          cdy=dy[i]
+          dL=arcLength[i]-arcLength[i-1]
+          d2x=0
+          d2y=0
+
+          if dL > ZERO_TOLERANCE:
+               d2x=(cdx-pdx)/dL
+               d2y=(cdy-pdy)/dL
+
+          K = 0
+          
+          if abs(cdx) >= ZERO_TOLERANCE or abs(cdy) >= ZERO_TOLERANCE:
+               K=(d2y*cdx-d2x*cdy)/math.pow((math.pow(cdx,2)+math.pow(cdy,2)),3/2)
+
+          curvature.append(K)
+          totalCurvature+=K
+
+     return curvature,totalCurvature
+  
+
+
+def computeAngles(curve):
+     angle=[]
+     angle.append(0.0);
+     totalAngleChange=0.0;
+
+     px=curve[0][0]
+     py=curve[0][1]
+     for i in range(1,len(curve)):
+          cx=curve[i][0]
+          cy=curve[i][1]
+          theta=math.atan2(cy-py,cx-px);
+          angle.append(theta);
+          px=cx
+          py=cy
+
+     if len(curve) >2 :
+          angle[0]=angle[1];
+          for i in range(1,len(angle)):
+               totalAngleChange += abs(angle[i]-angle[i-1]);
+
+     return totalAngleChange
+  
+def computeCurveStats(curve):
+
+     if len(curve)==0:
+          return 0.0,0.0,0.0
+     
+     arcLength,length=computeArcLength(curve)
+     dx,dy=computeDerivatives(curve)
+     _,totalCurvature=computeCurvatures(dx,dy,arcLength,len(curve))
+     totalAngleChange=computeAngles(curve)
+
+     return length,totalCurvature,totalAngleChange
+
+
+def compute_edge_stats():
+
+
+     for key,value in edge_to_samples.items():
+          
+          first_pt=node_mapping[int(key[0])].pt[0]
+          shock_curve=[edge_samples.get(int(value[id])).pt for id in range(1,len(value)-1)]
+          last_pt=node_mapping[int(key[1])].pt[0]
+          shock_curve.insert(0,first_pt)
+          shock_curve.append(last_pt)
+
+          plus_curve=[edge_samples.get(int(value[id])).plus_pt for id in range(1,len(value)-1)]
+          minus_curve=[edge_samples.get(int(value[id])).minus_pt for id in range(1,len(value)-1)]
+
+          
+          shock_length,shock_totalCurvature,shock_angle=computeCurveStats(shock_curve)
+          plus_length,plus_totalCurvature,plus_angle=computeCurveStats(plus_curve)
+          minus_length,minus_totalCurvature,minus_angle=computeCurveStats(minus_curve)
+
+          if len(plus_curve)==0:
+               plus_length         = shock_length
+               plus_totalCurvature = shock_totalCurvature
+               plus_angle          = shock_angle
+
+          if len(minus_curve)==0:
+               minus_length         = shock_length
+               minus_totalCurvature = shock_totalCurvature
+               minus_angle          = shock_angle
+
+          if len(plus_curve) and len(minus_curve):
+               poly=[]
+               poly.append(shock_curve[0])
+               poly.extend(plus_curve)
+               poly.append(shock_curve[-1])
+               minus_curve.reverse()
+               poly.extend(minus_curve)
+               poly.append(shock_curve[0])
+
+               
+          stats=CurveProps(SCurve=shock_totalCurvature,
+                           SLength=shock_length,
+                           SAngle=shock_angle,
+                           PCurve=plus_totalCurvature,
+                           PLength=plus_length,
+                           PAngle=plus_angle,
+                           MCurve=minus_totalCurvature,
+                           MLength=minus_length,
+                           MAngle=minus_angle,
+                           PolyArea=0.0)
+          
+          curve_stats[key]=stats
+
 
 def getLengthSampNode():
      length=0
@@ -31,7 +212,7 @@ def getLengthSampNode():
      
 def speedToPhi(speed):
      if speed != 0 and speed < 99990:
-          phi = acos(-1.0/speed);
+          phi = acos(-1.0/speed)
      else:
           phi = pi/2.0
      return phi
@@ -40,13 +221,13 @@ def fixAngleMPiPi_new(a):
      if a < -pi:
           return a+2.0*pi
      elif a > pi:
-          return a-2.0*pi;
+          return a-2.0*pi
      else:
           return a
 
 def translatePoint(pt,v,length):
-     x = pt[0] + length*cos(v);
-     y = pt[1] + length*sin(v);
+     x = pt[0] + length*cos(v)
+     y = pt[1] + length*sin(v)
      return (x,y)
 
 
@@ -110,12 +291,13 @@ def read_node_samples(sample_line,lines,sample_data,node_info):
           phi=speedToPhi(speed)
 
           #get reconstructed boundary points
-          left_bnd_pt  = translatePoint(pt,theta+phi, radius);
-          right_bnd_pt = translatePoint(pt,theta-phi, radius);
+          left_bnd_pt  = translatePoint(pt,theta+phi, radius)
+          right_bnd_pt = translatePoint(pt,theta-phi, radius)
 
           #get reconstructed boundary tangents
-          left_bnd_tangent  = fixAngleMPiPi_new(theta+phi-pi/2.0);
-          right_bnd_tangent = fixAngleMPiPi_new(theta-phi+pi/2.0);
+          left_bnd_tangent  = fixAngleMPiPi_new(theta+phi-pi/2.0)
+          right_bnd_tangent = fixAngleMPiPi_new(theta-phi+pi/2.0)
+
 
           #get affected node data
           ids=samp_to_node_mapping[id]
@@ -136,15 +318,78 @@ def read_node_samples(sample_line,lines,sample_data,node_info):
 def read_edge_header(sample_line,lines,sample_data,edge_offset,numb_edges):
      numb_mappings=getLengthSampNode()
      edge_start=sample_line+numb_mappings*sample_data+edge_offset
+     numb_samples=set()
+     degen_edges=0
      for idx in range(edge_start,edge_start+numb_edges):
           text=lines[idx]
           starts=[match.start() for match in re.finditer(re.escape('['), text)]
           end=[match.start() for match in re.finditer(re.escape(']'), text)]
           results=list(zip(starts,end))
           source,target=text[results[0][0]+1:results[0][1]].split(' ')
+          edge_samples=text[results[1][0]+1:results[1][1]].split(' ')
+          del edge_samples[-1]
+          if len(edge_samples)==1:
+               degen_edges+=1
           edge_mapping[int(source)].append(int(target))
+          edge_to_samples[(source,target)]=edge_samples
+          numb_samples.update(edge_samples)
+          
+     return len(numb_samples),degen_edges
 
+#process edge samples
+def read_edge_samples(sample_line,lines,sample_data,edge_offset,numb_edges,
+                      numb_edge_samples,degen_edges,node_info):
+     
+     numb_mappings=getLengthSampNode()
+     start=sample_line+numb_mappings*sample_data+edge_offset*2+numb_edges
+     end=start+(numb_edge_samples-degen_edges
+                -((numb_edges-degen_edges)*2))*sample_data
 
+     for ln in range(start,end,sample_data):
+          
+          idx=list(range(ln+1,ln+1+node_info))
+          
+          # get sample id
+          text=lines[idx[0]]
+          id=int(text.split(' ')[1])
+
+          # get location and radius
+          text=lines[idx[1]]
+          starts=[match.start() for match in re.finditer(re.escape('('), text)]
+          end=[match.start() for match in re.finditer(re.escape(')'), text)]
+          results=list(zip(starts,end))
+          x,y,radius=text[results[1][0]+1:results[1][1]].split(',')
+          pt=(float(x),float(y))
+          radius=float(radius)
+               
+          # get theta
+          text=lines[idx[5]]
+          theta=np.deg2rad(float(text.split(' ')[1]))
+
+          # get speed , convert to phi
+          text=lines[idx[6]]
+          speed=float(text.split(' ')[1])
+          phi=speedToPhi(speed)
+
+          #get reconstructed boundary points
+          left_bnd_pt  = translatePoint(pt,theta+phi, radius)
+          right_bnd_pt = translatePoint(pt,theta-phi, radius)
+
+          #get reconstructed boundary tangents
+          left_bnd_tangent  = fixAngleMPiPi_new(theta+phi-pi/2.0)
+          right_bnd_tangent = fixAngleMPiPi_new(theta-phi+pi/2.0)
+
+          samp=Sample(
+               pt=pt,
+               type=0.0,
+               radius=radius,
+               theta=theta,phi=phi,
+               plus_pt=left_bnd_pt,minus_pt=right_bnd_pt,
+               plus_theta=left_bnd_tangent, minus_theta=right_bnd_tangent)
+
+          edge_samples[id]=samp
+
+          
 def compute_sorted_order():
 
      # define a sorting order for nodes in graph
@@ -262,6 +507,26 @@ def compute_adj_feature_matrix(edge_features,NI,NJ):
                out=1.0
           feature_matrix[row][start]=out
 
+          start=28
+          for val in rad_list:
+               source=str(key)
+               target=str(adj_nodes_mapping[key][val])
+               pair=(source,target)
+               if pair not in curve_stats.keys():
+                    pair=(target,source)
+               props=curve_stats[pair]
+
+               feature_matrix[row][0+start]=props.SCurve
+               feature_matrix[row][1+start]=props.SLength
+               feature_matrix[row][2+start]=props.SAngle
+               feature_matrix[row][3+start]=props.PCurve
+               feature_matrix[row][4+start]=props.PLength
+               feature_matrix[row][5+start]=props.PAngle
+               feature_matrix[row][6+start]=props.PCurve
+               feature_matrix[row][7+start]=props.PLength
+               feature_matrix[row][8+start]=props.PAngle
+
+
      sorted_locations=sorted(locations,key=itemgetter(0,1),reverse=False)
      ul_corner=sorted_locations[0]
      lr_corner=sorted_locations[-1]
@@ -311,6 +576,7 @@ def convertEsfFile(esf_file,image_file):
      I=scipy.misc.imread(image_file)
      NI=I.shape[0]
      NJ=I.shape[1]
+     dims=np.array([NI,NJ])
      
      file=open(esf_file,'r')
      lines= file.read().splitlines() 
@@ -319,7 +585,7 @@ def convertEsfFile(esf_file,image_file):
      sample_data=12
      node_info=9
      edge_offset=4
-     edge_features=28
+     edge_features=37
      
      _,numb_nodes=lines[7].split(':')
      _,numb_edges=lines[8].split(':')
@@ -331,7 +597,12 @@ def convertEsfFile(esf_file,image_file):
 
      read_node_header(node_line,lines,numb_nodes)
      read_node_samples(sample_line,lines,sample_data,node_info)
-     read_edge_header(sample_line,lines,sample_data,edge_offset,numb_edges)
+     numb_edge_samples,numb_degen_edges=read_edge_header(sample_line,lines,
+                                                         sample_data,
+                                                         edge_offset,numb_edges)
+     read_edge_samples(sample_line,lines,sample_data,edge_offset,numb_edges,
+                       numb_edge_samples,numb_degen_edges,node_info)
+     compute_edge_stats()
      compute_sorted_order()
      adj_matrix,feature_matrix,ref_point=\
      compute_adj_feature_matrix(edge_features,NI,NJ)
@@ -344,7 +615,7 @@ def convertEsfFile(esf_file,image_file):
      hf.create_dataset('feature',data=feature_matrix)
      hf.create_dataset('adj_matrix',data=adj_matrix)
      hf.create_dataset('debug',data=ref_point)
-
+     hf.create_dataset('dims',data=dims)
      hf.close()
 
      
